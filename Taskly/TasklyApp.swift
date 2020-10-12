ff6a5680112f30cea27633445e7eeb02a62928f2
“maobui” <mao.buidinh@hotmail.com>
2020-10-12 21:42:23 +0700

    Init project

---
+++ Taskly/TasklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasklyApp.swift
+//  Taskly
+//
+//  Created by m.bui on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TasklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
