d1b93f73a2b93e7fb010823e2a1d46824c1649b2
“maobui” <mao.buidinh@hotmail.com>
2020-11-10 23:46:30 +0700

    [Update] Add favorite button in item.

--- Taskly/LandmarkDetail.swift
+++ Taskly/LandmarkDetail.swift
@@ -26,8 +26,23 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.landmards[landmarkIndex].isFavorite.toggle()
+                    }) {
+                        if self.userData.landmards[landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
                 HStack(alignment: .top) {
                     Text(landmark.park)
                         .font(.subheadline)
